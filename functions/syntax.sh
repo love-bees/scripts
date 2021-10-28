be4bf621856ef08f42a5fac19e374fa6eac3aefc
love-bees <33499899+love-bees@users.noreply.github.com>
2021-10-28 13:18:29 +0200

    various function syntax:

--- functions/syntax.sh
+++ functions/syntax.sh
@@ -1,20 +1,23 @@
-# syntax.sh
-
-function f1() {
+#Declaring functions using the reserved word function
+# Multiline
+function f1 {
 	echo Hello I\'m function 1
 	echo Bye!
 }
+# One line
+function f2 { echo Hello I\'m function 2; echo Bye!; }
 
-f2 () {
-	echo Hello I\'m function 2
+# Declaring functions without the function reserved word
+# Multiline
+f3 () { 
+	echo Hello I\'m function 3
 	echo Bye!
 }
-
-function f3() { echo Hello I\'m function 3; echo Bye!; }
-
+# One line
 f4 () { echo Hello I\'m function 4; echo Bye!; }
 
-f1
-f2
-f3
+# Invoking functions
 f4
+f3
+f2
+f1
